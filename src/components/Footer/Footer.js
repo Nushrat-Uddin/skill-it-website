f987c74c737ff422a7f56caf0d71276533b84fdf
Nushrat-Uddin <nushratuddin.nu@gmail.com>
2021-10-05 20:14:17 +0600

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <h4 className='mt-4'>Follow Us </h4>
             <i  class="fab fa-facebook-f i1"></i>
             <i class="fab fa-linkedin-in i1"></i>
-            <i class="fab fa-instagram"></i>
+            <i class="fab fa-instagram i1"></i>
 
             <p><small>© Copy 2021. All Rights Reserved</small></p>
         </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button,Carousel } from 'react-bootstrap';
 import './Home.css'
 
 const Home = () => {
@@ -13,6 +13,46 @@ const Home = () => {
     ,[])
     return (
         <div>
+         
+         <Carousel className='slide'>
+  <Carousel.Item>
+  <Carousel.Caption className='caption'>
+      <h1>Upgrade Skills In Online Academy</h1>
+      
+    </Carousel.Caption>
+    <img 
+      className="d-block w-100 h-25"
+      src="https://image.freepik.com/free-photo/close-up-hand-taking-notes_23-2148888827.jpg"
+      alt="First slide"
+    />
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100 h-25 "
+      src="https://image.freepik.com/free-vector/education-technology-futuristic-background-vector-gradient-blue-digital-remix_53876-114092.jpg"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h1>Discover A New Way Of Education</h1>
+     
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100 h-25"
+      src="https://image.freepik.com/free-photo/smiling-young-businessman_1098-778.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h1>Expand Your Career Oppotunities</h1>
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+
+         
            <h1 className="mt-5">Popular Catagories</h1><br/>
            <div className='homepage'>
            {
